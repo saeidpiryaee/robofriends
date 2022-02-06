a0fda9ae8fd8b1b382b408cda46b254e0002876f
saeidpiryaee <piryaeesaeid@gmail.com>
2022-02-06 18:35:55 +0330

    f commit

---
+++ src/Card.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Card = ({name, email, id}) => {
+   
+  return (
+     
+       
+      <div className = 'tc bg-light-green dib br3 pa3 ma2 grow bw2'>
+        <img src = {`https://robohash.org/${id}?200*200`}
+        
+        
+        alt = 'roboimg'/>
+
+
+        <div>
+        <h2>{name}</h2> 
+        <p>{email}</p>
+
+        </div>
+
+      </div>
+
+
+
+    
+      
+      
+      
+      
+      
+      
+      
+      
+
+  );
+
+            
+} 
+export default Card;
