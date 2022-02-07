98ddcfd226afd31de83ebf63ac9a62432a3a0046
saeidpiryaee <piryaeesaeid@gmail.com>
2022-02-07 13:54:58 +0330

    bring app component back

--- src/index.js
+++ src/index.js
@@ -3,22 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Card from './Card';
+// import Card from './Card';
 import tachyons from 'tachyons';
 import {robots} from './robots';
+// import CardList from './CardList';
+import App from './App';
 ReactDOM.render(
 
-<div>
-    
-<Card id = {robots[0].id}  name = {robots[0].name} email = {robots[0].email}/> 
-<Card id = {robots[1].id}  name = {robots[1].name} email = {robots[1].email}/>
-<Card id = {robots[2].id}  name = {robots[2].name} email = {robots[2].email}/>
 
 
-    
-</div>
-
-
+   <App robots = {robots}/>
 
     
     
